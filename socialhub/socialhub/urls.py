9ae94d1fc5190cfb9fb7d0bb82079320bd310e6d
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-04-29 14:23:48 +0200

    Site will still not load after deleting code in urls.py.

--- socialhub/socialhub/urls.py
+++ socialhub/socialhub/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = ('',
-	url(r'^$', include('theme.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     # Examples:
