8df2c55201af322a1dfac37b192d1f9848519086
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-04-29 14:21:16 +0200

    deleted info in urls.py to try to start back, but frontpage will still not load.

--- socialhub/theme/urls.py
+++ socialhub/theme/urls.py
@@ -1,7 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from theme import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.frontpage, name='frontpage'),
-)
