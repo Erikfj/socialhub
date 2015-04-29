60d8202b828b1d89a71cd8e51faf8c012179e4d6
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-04-29 14:36:05 +0200

    Fixed lazy error, resulting i faulty site. fixed urlpattern = patterns

--- socialhub/socialhub/urls.py
+++ socialhub/socialhub/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = ('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-
     # Examples:
     # url(r'^$', 'socialhub.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
