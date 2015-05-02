709af229ba40be9668bf41f1a9626e103ee1a547
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-02 12:40:11 +0200

    Started implementing model for each hub / note in models.py

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class HubNote(models.Model):
+	user = models.ForeignKey(User, related_name='notes')
+	note = models.TextField()
+	points = models.PositiveIntegerField(default=0)
+	created_by = models.ForeignKey(User, related_name="created_notes")
+	created_datetime = models.DateTimeField()
+
+	def __unicode__(self):
+		return u'%s' % self.note
+
+		class Meta:
+			ordering = ['-id']
