1c75fa3b795e92462a5c582501a779c4d8217908
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-02 13:10:29 +0200

    added code and import neccesary for creating a functional view.

--- socialhub/hubs/views.py
+++ socialhub/hubs/views.py
@@ -1,3 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.utils import timezone
+
+from hubs.models import Hub
+from hubs.models import HubNote
+
 
 # Create your views here.
+def hub_listing(request):
+    hubs = Hub.objects.all()
+    context = {'hubs': hubs}
+    return render(request, 'hubs/hub_listing.html', context)
+
+def hub_details(request, hub_id):
+	# this view renders hub details and all notes saved to the hub.
+	hub = Hub.objects.get(pk=hub_id)
