e9d98bc28df86efe75a81320ffb778b1a67bf0a7
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-02 13:26:42 +0200

    added the model Hub and renamed User in HubNote to hub.

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Hub(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    hobby = models.CharField(max_length=255)
+    owned_points = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.name, self.email)
+
 class HubNote(models.Model):
-	user = models.ForeignKey(User, related_name='notes')
+	hub = models.ForeignKey(Hub, related_name='notes')
 	note = models.TextField()
 	points = models.PositiveIntegerField(default=0)
 	created_by = models.ForeignKey(User, related_name="created_notes")
