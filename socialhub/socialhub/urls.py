d66ba566df9e11aee3e0476257caeb202eb9aea7
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-02 13:11:23 +0200

    no major changes. removed comments and blank spaces/lines

--- socialhub/socialhub/urls.py
+++ socialhub/socialhub/urls.py
@@ -5,7 +5,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^hubs/', include('hubs.urls')),
-    # Examples:
-    # url(r'^$', 'socialhub.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+
+
+    
 )
