1c05d245d3aa30a2fc76ce7b60e933d49daad055
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-22 14:06:35 +0200

    Added admin functionality.

--- socialhub/hubs/admin.py
+++ socialhub/hubs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from hubs.models import Hub
+from hubs.models import HubNote
+
+admin.site.register(Hub)
+admin.site.register(HubNote)
