34be6d3f7eddc3123d4107edfaf6d4f9c60f6469
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-22 13:10:58 +0200

    added note creation code to views.py.

--- socialhub/hubs/views.py
+++ socialhub/hubs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from hubs.models import Hub
 from hubs.models import HubNote
@@ -15,5 +16,33 @@ def hub_listing(request):
     return render(request, 'hubs/hub_listing.html', context)
 
 def hub_details(request, hub_id):
-	# this view renders hub details and all notes saved to the hub.
 	hub = Hub.objects.get(pk=hub_id)
+
+	if request.method == "POST":
+		new_note_text = request.POST.get('new_note')
+		new_note = HubNote()
+		new_note.note = new_note_text
+		new_note.hub = hub
+		new_note.created_by = request.user
+		new_note.created_datetime = timezone.now()
+		new_note.save()
+
+	notes = hub.notes.all()
+	page_number = request.GET.get('page')
+	pageinator = Paginator(notes, 5)
+	try:
+		notes = pageinator.page(page_number)
+	except PageNotAnInteger:
+		notes = pageinator.page(1)
+	except EmptyPage:
+		notes = paginator.page(pageinator.num_pages)
+
+
+	context = {
+		'hub': hub,
+		'notes': notes}
+	return render(request, 'hubs/hub_details.html', context)
+
+
+
+
