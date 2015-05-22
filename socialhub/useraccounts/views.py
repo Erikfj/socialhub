a8fde10ae1b0f06b2685fbf3e96ee7af51571d2d
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-22 11:48:13 +0200

    started coding views.py for testing. set up the login code.

--- socialhub/useraccounts/views.py
+++ socialhub/useraccounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def user_login(request):
+	context = {}
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password'] 
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('frontpage')
+		else:
+			context['login_failed'] = True
+	return render(request, 'useraccounts/login.html', context)
