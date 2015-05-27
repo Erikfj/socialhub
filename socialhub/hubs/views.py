a16ab2eaa8666558f8924f1d67623058321d02ce
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-27 13:41:55 +0200

    updated views.py for compatibility.

--- socialhub/hubs/views.py
+++ socialhub/hubs/views.py
@@ -15,41 +15,40 @@ def hub_listing(request):
     context = {'hubs': hubs}
     return render(request, 'hubs/hub_listing.html', context)
 
+
 def hub_details(request, hub_id):
-	hub = Hub.objects.get(pk=hub_id)
+    hub = Hub.objects.get(pk=hub_id)
 
-	if request.method == "POST":
-		new_note_text = request.POST.get('new_note')
-		new_note = HubNote()
-		new_note.note = new_note_text
-		new_note.hub = hub
-		new_note.created_by = request.user
-		new_note.created_datetime = timezone.now()
-		new_note.save()
+    if request.method == "POST":
+        new_note_text = request.POST.get('new_note')
+        new_note = HubNote()
+        new_note.note = new_note_text
+        new_note.hub = hub
+        new_note.created_by = request.user
+        new_note.created_datetime = timezone.now()
+        new_note.save()
 
-	notes = hub.notes.all()
-	page_number = request.GET.get('page')
-	pageinator = Paginator(notes, 5)
-	try:
-		notes = pageinator.page(page_number)
-	except PageNotAnInteger:
-		notes = pageinator.page(1)
-	except EmptyPage:
-		notes = paginator.page(pageinator.num_pages)
+    # notes = hub.notes.all() Det kom en feil her da hub ikke har noe attributt notes. men note
+    notes = HubNote.objects.all()
+    page_number = request.GET.get('page')
+    pageinator = Paginator(notes, 5)
+    try:
+        notes = pageinator.page(page_number)
+    except PageNotAnInteger:
+        notes = pageinator.page(1)
+    except EmptyPage:
+        notes = pageinator.page(pageinator.num_pages)
 
+    context = {
+        'hub': hub,
+        'notes': notes}
+    return render(request, 'hubs/hub_details.html', context)
 
-	context = {
-		'hub': hub,
-		'notes': notes}
-	return render(request, 'hubs/hub_details.html', context)
 
 @csrf_exempt
 def student_note_add_points(request, student_id, note_id):
-	note = StudentNote.objects.get(pk=note_id)
-	note.points = note.points + 1
-	note.save()
-	data = {'points_updated': note.points}
-	return JsonResponse(data)
-
-
-
+    note = StudentNote.objects.get(pk=note_id)
+    note.points = note.points + 1
+    note.save()
+    data = {'points_updated': note.points}
+    return JsonResponse(data)
