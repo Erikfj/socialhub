8ee857de3bc034b6e901fb39d7fce25e63481080
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-27 14:16:52 +0200

    removed datetime in createddeatetime as it led to errors.

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -6,7 +6,6 @@ class Hub(models.Model):
     username = models.ForeignKey(User, related_name='notes')
     topic = models.CharField(max_length=100)
     hubpoints = models.PositiveIntegerField(default=0)
-    created_datetime = models.DateTimeField()
 
     def __unicode__(self):
         return u'%s' % self.note
