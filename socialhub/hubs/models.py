e84194c499681776f1b9de03cc2fc861350d4904
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-27 14:04:02 +0200

    made changes to related name so as Hub and HubNote not clash.

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -8,7 +8,7 @@ class Hub(models.Model):
     hubpoints = models.PositiveIntegerField(default=0)
     created_by = models.ForeignKey(User, related_name="created_notes")
     created_datetime = models.DateTimeField()
-    
+
     def __unicode__(self):
         return u'%s (%s)' % (self.topic, self.message)
 
@@ -17,8 +17,8 @@ class HubNote(models.Model):
     user = models.ForeignKey(User, related_name='notes_2')
     note = models.TextField()
     points = models.PositiveIntegerField(default=0)
-    created_by = models.ForeignKey(User, related_name="created_notes")
-    created_datetime = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name="created_notes_2")
+    created_datetime = models.TextField()
 
     def __unicode__(self):
         return u'%s' % self.note
