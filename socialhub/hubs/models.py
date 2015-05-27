f99fa378c5f13c8b12b120fe1ea23678d7a965d2
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-27 13:37:26 +0200

    updated models.py to include more options.

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Hub(models.Model):
     topic = models.CharField(max_length=100)
     email = models.TextField()
+    username = models.ForeignKey(User, related_name='notes')
 
     def __unicode__(self):
         return u'%s (%s)' % (self.topic, self.email)
 
+
 class HubNote(models.Model):
-	user = models.ForeignKey(User, related_name='notes')
-	note = models.TextField()
-	points = models.PositiveIntegerField(default=0)
-	created_by = models.ForeignKey(User, related_name="created_notes")
-	created_datetime = models.DateTimeField()
+    user = models.ForeignKey(User, related_name='notes_2')
+    note = models.TextField()
+    points = models.PositiveIntegerField(default=0)
+    created_by = models.ForeignKey(User, related_name="created_notes")
+    created_datetime = models.DateTimeField()
 
-	def __unicode__(self):
-		return u'%s' % self.note
+    def __unicode__(self):
+        return u'%s' % self.note
 
-		class Meta:
-			ordering = ['-id']
+    class Meta:
+        ordering = ['-id']
 
 
 
