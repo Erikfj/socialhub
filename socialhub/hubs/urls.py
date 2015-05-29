7476aeebc65e971e60da74a368093850eb11d603
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-29 21:15:50 +0200

    changed values of points system to user_topic

--- socialhub/hubs/urls.py
+++ socialhub/hubs/urls.py
@@ -5,6 +5,6 @@ from hubs import views
 urlpatterns = patterns('',
     url(r'^$', views.hub_listing, name='hub_listing'),
     url(r'^(\d+)/$', views.hub_details, name='hub_details'),
-    url(r'^(\d+)/add_points/(\d+)$', views.student_note_add_points, 
-    	name='student_note_add_points'),
+    url(r'^(\d+)/add_points/(\d+)$', views.user_topic_add_points, 
+    	name='user_topic_add_points'),
 )
