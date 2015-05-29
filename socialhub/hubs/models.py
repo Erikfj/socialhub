2f234ed6104f2bd8d0f6ee0a84bf4f1ac73c9d20
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-29 15:47:50 +0200

    added created by and datetime properly with the right DateTimeField.

--- socialhub/hubs/models.py
+++ socialhub/hubs/models.py
@@ -6,7 +6,8 @@ class Hub(models.Model):
     username = models.ForeignKey(User, related_name='notes')
     topic = models.CharField(max_length=100)
     hubpoints = models.PositiveIntegerField(default=0)
-    
+    created_by = models.ForeignKey(User, related_name="created_notes")
+    created_datetime = models.DateTimeField()
 
     def __unicode__(self):
         return u'%s, %s' % (self.topic, self.hubpoints)
@@ -19,7 +20,7 @@ class HubNote(models.Model):
     note = models.TextField()
     points = models.PositiveIntegerField(default=0)
     created_by = models.ForeignKey(User, related_name="created_notes_2")
-    created_datetime = models.TextField()
+    created_datetime = models.DateTimeField()
 
     def __unicode__(self):
         return u'%s' % self.note
