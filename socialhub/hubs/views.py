31c9f4f9582be3a3b2c41e8c96435e9838cba944
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-29 21:50:01 +0200

    changed values to hub as this should be the correct values.

--- socialhub/hubs/views.py
+++ socialhub/hubs/views.py
@@ -50,9 +50,9 @@ def hub_details(request, hub_id):
 
 
 @csrf_exempt
-def user_topic_add_points(request, username_id, topic_id):
-    topic = UserTopic.objects.get(pk=topic_id)
-    topic.points = topic.points + 1
-    topic.save()
-    data = {'points_updated': topic.points}
+def hub_topic_add_points(request, user_id, hub_id):
+    hub = HubTopic.objects.get(pk=hub_id)
+    hub.points = hub.points + 1
+    hub.save()
+    data = {'points_updated': hub.points}
     return JsonResponse(data)
