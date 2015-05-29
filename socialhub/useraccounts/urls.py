190663f3c16c97497350d978e5e124e6f4e9c8fb
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-29 22:39:05 +0200

    added the settings url instead of account.

--- socialhub/useraccounts/urls.py
+++ socialhub/useraccounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
 	url(r'^login$', views.user_login, name='user_login'),
 	url(r'^register$', views.user_register, name='user_register'),
 	url(r'^logout$', views.user_logout, name='user_logout'),
-	url(r'^account$', views.user_account, name='user_account'),
+	url(r'^settings$', views.user_settings, name='user_settings'),
 
 	)
