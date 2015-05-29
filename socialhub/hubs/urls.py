bc30f483a62dfe6c020ffc3cdc1af1d3a3528b3d
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-05-29 23:43:45 +0200

    changed hub_topic_add_points to username_topic etc.

--- socialhub/hubs/urls.py
+++ socialhub/hubs/urls.py
@@ -5,6 +5,6 @@ from hubs import views
 urlpatterns = patterns('',
     url(r'^$', views.hub_listing, name='hub_listing'),
     url(r'^(\d+)/$', views.hub_details, name='hub_details'),
-    url(r'^(\d+)/add_points/(\d+)$', views.hub_topic_add_points, 
-    	name='hub_topic_add_points'),
+    url(r'^(\d+)/add_points/(\d+)$', views.username_topic_add_points, 
+    	name='username_topic_add_points'),
 )
